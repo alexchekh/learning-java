c398404a63a07275a33c93e3e3e495323b6056e6
alexchekh <alex.d.cheh@gmail.com>
2017-09-06 20:13:54 +0500

    test

---
+++ src/ru/geekbrains/guess_the_number/Main.java
@@ -0,0 +1,39 @@
+package ru.geekbrains.guess_the_number;
+
+import java.util.Scanner;
+
+import static java.util.Scanner.*;
+
+public class Main {
+
+    private static  Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("Ваша задача угадать число");
+
+        for (int i=10; i <= 30; i += 10) playlevel(i);
+        System.out.println("Вы выиграли!");
+
+
+
+        scanner.close();
+
+    }
+
+    private static void playlevel (int range){
+        int number  = (int) (Math.random() * range);
+        while (true){
+            System.out.println("Угадайте число от 0 до " + range);
+            int input_number = scanner.nextInt();
+            if (input_number == number){
+                System.out.println("Вы угадали");
+                break;
+            } else if (input_number > number){
+                System.out.println("Загаданное число меньше");
+            } else {
+                System.out.println("Загаданное чило больше");
+            }
+        }
+    }
+}
